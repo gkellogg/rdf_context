aaf5b2e43402dea68b75b4a7bdf380d332b379f0
Gregg Kellogg <gregg@kellogg-assoc.com>
2010-09-11 22:52:17 -0700

    RDFa profile specs.

--- spec/rdfa_parser_spec.rb
+++ spec/rdfa_parser_spec.rb
@@ -77,7 +77,6 @@ describe "RDFa parser" do
     xml.should include("E = mc<sup xmlns=\"http://www.w3.org/1999/xhtml\">2</sup>: The Most Urgent Problem of Our Time")
   end
 
-
   it "should parse BNodes" do
     sampledoc = <<-EOF
     <?xml version="1.0" encoding="UTF-8"?>
@@ -106,6 +105,113 @@ describe "RDFa parser" do
     
     xml.should include("Ralph Swick")
     xml.should include("Manu Sporny")
+  end
+  
+  describe :profiles do
+    before(:all) do
+      @prof = %(<?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE html>
+      <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+          <title>Test mappings</title>
+        </head>
+        <body prefix="rdfa: http://www.w3.org/ns/rdfa#">
+          <p typeof=""><span property="rdfa:uri">#{DC_NS.uri}</span><span property="rdfa:prefix">dc</span></p>
+          <p typeof=""><span property="rdfa:uri">#{DC_NS.title}</span><span property="rdfa:term">title</span></p>
+        </body>
+      </html>
+      )
+      @doc = %(<?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE html>
+      <html xmlns="http://www.w3.org/1999/xhtml">
+        <body profile="http://example.com/profile">
+          <div about ="http://example.com/doc" typeof="dc:Agent">
+            <p property="title">A particular agent</p>
+          </div>
+        </body>
+      </html>
+      )
+    end
+
+    before(:each) do
+      @profile_graph = ConjunctiveGraph.new(:store => MemoryStore.new)
+      @parser = RdfaParser.new(:profile_graph => @profile_graph)
+      OpenURI.stub!(:open_uri).with("http://example.com/profile").and_return(@prof)
+    end
+    
+    describe "new profile" do
+      before(:each) do
+        # Clear vocabulary cache
+        #RdfContext.debug = true
+        RdfaParser.send(:class_variable_set, :@@vocabulary_cache, {})
+        @parser.parse(@doc, "http://example.com/doc")
+        #RdfContext.debug = false
+      end
+      
+      describe "profile graph" do
+        it "should have context http://example.com/profile" do
+          @profile_graph.contexts.map(&:identifier).should include("http://example.com/profile")
+        end
+      end
+      
+      describe "processed graph" do
+        it "should have type dc:Agent" do
+          @parser.graph.should be_contains(Triple.new("http://example.com/doc", RDF_TYPE, DC_NS.Agent))
+        end
+      
+        it "should have property dc:title" do
+          @parser.graph.should be_contains(Triple.new("http://example.com/doc", DC_NS.title, nil))
+        end
+      end
+    end
+    
+    describe "cached profile" do
+      before(:each) do
+        # Clear vocabulary cache
+        RdfaParser.send(:class_variable_set, :@@vocabulary_cache, {})
+        @parser.parse(@doc, "http://example.com/doc")
+      end
+      
+      it "should not re-parse profile" do
+        RdfaParser.send(:class_variable_set, :@@vocabulary_cache, {})
+        Parser.should_not_receive(:parse).with(@prof, "http://example.com/profile", :profile_graph => @parser.profile_graph).and_return(@prof_graph)
+        RdfaParser.new.parse(@doc, "http://example.com/doc")
+      end
+      
+      it "should create vocab_cache" do
+        RdfaParser.send(:class_variable_get, :@@vocabulary_cache).should be_a(Hash)
+      end
+      
+    end
+    
+    describe "profile content" do
+      before(:each) do
+        @prof_graph = Graph.new
+        bn_p = BNode.new("prefix")
+        bn_t = BNode.new("term")
+        @prof_graph.add(
+          Triple.new(bn_p, RDFA_NS.prefix_, "dc"),
+          Triple.new(bn_p, RDFA_NS.uri_, Literal.untyped(DC_NS.uri.to_s)),
+          Triple.new(bn_t, RDFA_NS.term_, "title"),
+          Triple.new(bn_t, RDFA_NS.uri_, Literal.untyped(DC_NS.title.to_s))
+        )
+        Parser.should_receive(:parse).with(@prof, "http://example.com/profile", :profile_graph => @profile_graph, :graph => instance_of(Graph)).and_return(@prof_graph)
+        
+        # Clear vocabulary cache
+        RdfaParser.send(:class_variable_set, :@@vocabulary_cache, {})
+        #RdfContext.debug = true
+        @parser.parse(@doc, "http://example.com/doc")
+        #RdfContext.debug = false
+      end
+      
+      it "should have type dc:Agent" do
+        @parser.graph.should be_contains(Triple.new("http://example.com/doc", RDF_TYPE, DC_NS.Agent))
+      end
+      
+      it "should have property dc:title" do
+        @parser.graph.should be_contains(Triple.new("http://example.com/doc", DC_NS.title, nil))
+      end
+    end
   end
   
   def self.test_cases(suite)
