308d2ea0c16d4f1665691fd39572629d6803e01f
Gregg Kellogg <gregg@kellogg-assoc.com>
2010-12-14 19:18:49 -0800

    Added Resource class, a common super-class of BNode, Graph, Literal and URIRef.
    Implemented Resource.parse and variations for sub-classes to parse N3-coded resources.

---
+++ lib/rdf_context/resource.rb
@@ -0,0 +1,56 @@
+module RdfContext
+  # Common super-class for things that are RDF Resources
+  class Resource
+
+    ##
+    # Returns `true` to indicate that this value is a resource.
+    #
+    # @return [Boolean]
+    def resource?
+      true
+    end
+
+    ##
+    # Returns `false`, overridden in Literal
+    #
+    # @return [Boolean]
+    def literal?
+      false
+    end
+
+    ##
+    # Returns `false`, overridden in URIref
+    #
+    # @return [Boolean]
+    def uri?
+      false
+    end
+
+    ##
+    # Returns `false`, overridden in BNode
+    #
+    # @return [Boolean]
+    def bnode?
+      false
+    end
+    
+    ##
+    # Returns `false`, overridden in BNode
+    #
+    # @return [Boolean]
+    def graph?
+      false
+    end
+    
+    # Parse a string to a resource, in NTriples format
+    def self.parse(str)
+      case str
+      when /^_:/    then BNode.parse(str)
+      when /^<"/    then URIRef.parse(str)
+      when /^http:/ then URIRef.parse(str)
+      when /^\"/    then Literal.parse(str)
+      else               Literal.parse(str)
+      end
+    end
+  end
+end
